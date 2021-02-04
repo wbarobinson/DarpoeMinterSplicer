10ba73dbda8cfe2588c75ab523a8b11216b17bc5
William <wbarobinson@gmail.com>
2021-02-04 15:02:35 -0500

    server and html for testing

---
+++ frontend/express-sendfile/server.js
@@ -0,0 +1,13 @@
+//A simple server for pushing index.html to localhost
+//Without this, metamask wont connect
+
+var express = require('express');
+var app = express();
+var path = require('path');
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(8080);
