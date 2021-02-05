ed595963e2e0239b755012b4258e009e9dd48dea
William <wbarobinson@gmail.com>
2021-02-05 11:26:46 -0500

    deployed newest contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,4 +5,10 @@ require("@nomiclabs/hardhat-waffle");
  */
 module.exports = {
   solidity: "0.7.3",
+  networks: {
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/d2e662258db945128d743db512f95542`,
+      accounts: [`0x72eb6b67032d2e4e2aeb6af0767bbd187558d8ea9851ee58d98a0b26a392a631`]
+    }
+  }
 };
